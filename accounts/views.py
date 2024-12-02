4a01d8bf112443a97288d820032200fa3cf3cb0c
Mike Shmygel <m.shmygel@gmail.com>
2024-12-02 14:48:16 +0200

    add app accounts

---
+++ accounts/views.py
@@ -0,0 +1,24 @@
+# from django.contrib.auth.views import LoginView, LogoutView
+# from django.urls import reverse_lazy
+#
+#
+# class CustomLoginView(LoginView):
+#     template_name = "registration/login.html"
+#     redirect_authenticated_user = True
+#     next_page = "/"
+#     success_url = reverse_lazy("kitchen")
+#
+# class CustomLogoutView(LogoutView):
+#     template_name = "registration/logged_out.html"
+
+
+# from django.contrib.auth.views import LoginView, LogoutView
+# from django.urls import reverse_lazy
+#
+# class CustomLoginView(LoginView):
+#     template_name = "registration/login.html"
+#     redirect_authenticated_user = True
+#     success_url = reverse_lazy("kitchen")
+#
+# class CustomLogoutView(LogoutView):
+#     template_name = "registration/logged_out.html"
