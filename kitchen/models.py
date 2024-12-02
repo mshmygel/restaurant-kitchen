274307af164445500f253cfc852b990ea556139a
Mike Shmygel <m.shmygel@gmail.com>
2024-12-02 15:03:40 +0200

    implement kitchen models

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+class Cook(User):
+    years_of_experience = models.IntegerField(null=False, blank=False)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType,on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    def __str__(self):
+        return f"{self.name}, price: {self.price}, description: {self.description}"
+
