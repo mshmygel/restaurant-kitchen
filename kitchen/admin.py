ba8bc8796e1a06e5ed411de3edd12c23c6a82b07
Mike Shmygel <m.shmygel@gmail.com>
2024-12-03 17:07:32 +0200

    change admin.py

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
-from .models import DishType, Cook, Dish
+from django.contrib.auth.admin import UserAdmin
+from .models import Dish, DishType
+from accounts.models import Cook
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("dish_type",)
 
 
 admin.site.register(DishType)
-admin.site.register(Cook)
-admin.site.register(Dish)
