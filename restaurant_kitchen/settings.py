cb21e082075de826bcc94fafa58c908bebd37181
Mike Shmygel <m.shmygel@gmail.com>
2024-12-03 17:04:35 +0200

    change settings

--- restaurant_kitchen/settings.py
+++ restaurant_kitchen/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "accounts.User"
+AUTH_USER_MODEL = "accounts.Cook"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
