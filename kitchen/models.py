5d62dba8a72166bf8d8266e8f3d5ec98fae98fa8
Mike Shmygel <m.shmygel@gmail.com>
2024-12-03 17:02:16 +0200

    change models and recreate DB

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.db import models
-from accounts.models import User
+from django.urls import reverse
+
+from accounts.models import Cook
 
 
 class DishType(models.Model):
@@ -11,24 +14,17 @@ class DishType(models.Model):
     def __str__(self):
         return self.name
 
-class Cook(User):
-    years_of_experience = models.IntegerField(null=False, blank=False)
-
-    class Meta:
-        verbose_name = "cook"
-        verbose_name_plural = "cooks"
-
-    def __str__(self):
-        return f"{self.username} ({self.first_name} {self.last_name})"
-
 
 class Dish(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    dish_type = models.ForeignKey(DishType,on_delete=models.CASCADE)
-    cooks = models.ManyToManyField(Cook, related_name="dishes")
+    dish_type = models.ForeignKey(DishType,related_name="dishes", on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
 
     def __str__(self):
         return f"{self.name}, price: {self.price}, description: {self.description}"
+
+    def get_absolute_url(self):
+        return reverse("kitchen:dish-detail", kwargs={"pk": self.pk})
 
