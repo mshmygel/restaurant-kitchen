dd8b3051ecfe8139d436ddec414f449194dfd21c
Mike Shmygel <m.shmygel@gmail.com>
2024-12-05 22:07:59 +0200

    change dev, prod

--- restaurant_kitchen/settings/dev.py
+++ restaurant_kitchen/settings/dev.py
@@ -3,7 +3,8 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
