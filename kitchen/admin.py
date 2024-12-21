2c94f2723aea6590b1c3b6e31cebd77986c70029
Mike Shmygel <m.shmygel@gmail.com>
2024-12-21 14:22:46 +0200

    removed Cook register from kitchen/admin.py

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,29 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import Dish, DishType
-from accounts.models import Cook
-
-
-@admin.register(Cook)
-class CookAdmin(UserAdmin):
-    list_display = UserAdmin.list_display + ("years_of_experience",)
-    fieldsets = UserAdmin.fieldsets + (
-        (("Additional info", {"fields": ("years_of_experience",)}),)
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (
-            (
-                "Additional info",
-                {
-                    "fields": (
-                        "first_name",
-                        "last_name",
-                        "years_of_experience",
-                    )
-                },
-            ),
-        )
-    )
 
 
 @admin.register(Dish)
